6c272d81264c9d366312441a30d9c8bcf4a7a99c
Erik James Robles <erikrobles@Eriks-MacBook-Air.local>
2025-01-12 17:21:18 -0600

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS to allow frontend communication
-  app.enableCors();
+  app.enableCors({
+    origin:
+      'https://6784418a064c6d0008452513--blissful-turing-be15a5.netlify.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-  // Set the port to 5000
-  await app.listen(5000);
+  await app.listen(5000); // Ensure your backend is listening on port 5000
 }
 bootstrap();
