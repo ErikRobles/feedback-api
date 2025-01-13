bc34315c58e6389693e600996d100e2a6d13af79
Erik James Robles <erikrobles@Eriks-MacBook-Air.local>
2025-01-12 22:13:39 -0600

    Added cors

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [
       'http://localhost:5173', // For local development
-      'https://6784418a064c6d0008452513--blissful-turing-be15a5.netlify.app', // For production
+      'https://6784418a064c6d0008452513--blissful-turing-be15a5.netlify.app', // Your Netlify domain
     ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Auth-Password'],
+    credentials: true, // Allow credentials like cookies or Authorization headers
   });
 
-  // Apply AuthMiddleware only to feedback routes (excluding /verify-password)
-
-  await app.listen(5000); // Ensure your backend is listening on port 5000
+  await app.listen(5000);
 }
 bootstrap();
